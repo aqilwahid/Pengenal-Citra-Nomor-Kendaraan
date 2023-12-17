ea4f4862df9f0a983e6f32a37f4c8d53c285a232
Aqil <aqilwahid@gmail.com>
2023-12-17 14:41:23 +0700

    Summary Update HTML

--- WebApp/app.py
+++ WebApp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 # webserver getway interface
 app = Flask(__name__)
@@ -6,8 +6,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello World"
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
