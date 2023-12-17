e2193738482b360bc9d983cadb7d3214372526f5
Aqil <aqilwahid@gmail.com>
2023-12-17 21:38:18 +0700

    update

--- WebApp/app.py
+++ WebApp/app.py
@@ -1,11 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import os
 
 # webserver getway interface
 app = Flask(__name__)
 
+BASE_PATH = os.getcwd()
+UPLOAD_PATH = os.path.join(BASE_PATH, "/static/upload/")
 
-@app.route("/")
+
+@app.route("/", methods=["POST", "GET"])
 def index():
+    if request.method == "POST":
+        upload_file = request.files["image_name"]
+        filename = upload_file.filename
+        path_save = os.path.join(UPLOAD_PATH, filename)
+        upload_file.save(path_save)
+
+        return render_template("index.html")
+
     return render_template("index.html")
 
 
